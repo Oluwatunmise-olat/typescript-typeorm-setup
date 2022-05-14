be7c4efea503dbfa4efb05a1a88c28b4531a5c00
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-05-14 01:47:58 +0100

    Make routes Protected

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import postsController from "../controllers/posts.controllers";
+import { authenticate } from "../middlewares/auth.middleware";
 import RequestBodyDataMiddleware from "../middlewares/bodyfield.middleware";
 import { createPostValidator } from "../validators/post.validator";
 
@@ -8,13 +9,14 @@ const router = Router();
 
 router
   .route("/")
-  .get(postsController.getOne)
-  .delete(postsController.delete)
+  .get(authenticate, postsController.getOne)
+  .delete(authenticate, postsController.delete)
   .post(
+    authenticate,
     RequestBodyDataMiddleware.validate(createPostValidator),
     postsController.create
   );
 
-router.get("/all", postsController.getAll);
+router.get("/all", authenticate, postsController.getAll);
 
 export default router;
