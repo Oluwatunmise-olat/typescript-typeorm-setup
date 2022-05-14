47bb7c982e3dc0d887b280765e32930a98dc67df
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2022-05-14 18:46:03 +0100

    Rebased

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -22,13 +22,10 @@ router
     postsController.create
   );
 
-<<<<<<< HEAD
-=======
 router.get(
   "/all",
   passport.authenticate("jwt", { failWithError: true }),
   postsController.getAll
 );
 
->>>>>>> 4d376d6 (Configure s3 bcket requiremenets)
 export default router;
